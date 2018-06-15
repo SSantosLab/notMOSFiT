7e618a13cb6bdd48376986d74d0401e981518705
Kamile Lukosiute <klukosiu@wellesley.edu>
2018-06-15 11:37:23 -0500

    add citation comments

--- mosfit/modules/engines/rprocess.py
+++ mosfit/modules/engines/rprocess.py
@@ -18,7 +18,7 @@ class RProcess(Engine):
     """
 
     _REFERENCES = [
-        {SOURCE.BIBCODE: '2013ApJ...775...18B'},
+        {SOURCE.BIBCODE: '2016ApJ...829..110B'},
         {SOURCE.BIBCODE: '2017LRR....20....3M'},
         {SOURCE.BIBCODE: '2017arXiv170708132V'}
     ]
@@ -28,16 +28,26 @@ class RProcess(Engine):
     def __init__(self, **kwargs):
         """Initialize module."""
         super(Engine, self).__init__(**kwargs)
-        self._wants_dense = True
+	self._wants_dense = True # This function wants a dense times array
+	
+	# Barnes simulation results from 2016ApJ...829..110B (Barnes and Kasen)
+	# v = ejecta velocity, M = ejecta mass
         barnes_v = np.asarray([0.1, 0.2, 0.3])
         barnes_M = np.asarray([1.e-3, 5.e-3, 1.e-2, 5.e-2])
+	
+	# a,b,d are parameterizations of the overall thermalization efficency
+	# laid out in 2016ApJ...829..110B: Eqn. 34
+	
+	# The numbers given here are for simulation results in
+	# 2016ApJ...829..110B: Table 1
         barnes_a = np.asarray([[2.01, 4.52, 8.16], [0.81, 1.9, 3.2], [
                               0.56, 1.31, 2.19], [.27, .55, .95]])
         barnes_b = np.asarray([[0.28, 0.62, 1.19], [0.19, 0.28, 0.45], [
                               0.17, 0.21, 0.31], [0.10, 0.13, 0.15]])
         barnes_d = np.asarray([[1.12, 1.39, 1.52], [0.86, 1.21, 1.39], [
                               0.74, 1.13, 1.32], [0.6, 0.9, 1.13]])
-
+	
+	# To get values for others in between, we interpolate
         self.therm_func_a = RegularGridInterpolator(
             (barnes_M, barnes_v), barnes_a, bounds_error=False, fill_value=None)
         self.therm_func_b = RegularGridInterpolator(
@@ -47,11 +57,15 @@ class RProcess(Engine):
 
     def process(self, **kwargs):
         """Process module."""
+	# To get luminosity out, pass a times array, a mass, a t_explosion, and a
+	# v_ejecta
         self._times = kwargs[self.key('dense_times')]
         self._mass = kwargs[self.key('mejecta')] * M_SUN_CGS
         self._rest_texplosion = kwargs[self.key('resttexplosion')]
         self._vejecta = kwargs[self.key('vejecta')]
-        self._a = self.therm_func_a(
+        
+	# These are the interpolator functions interpoalted for a, 2b, and d
+	self._a = self.therm_func_a(
             [self._mass / M_SUN_CGS, self._vejecta / self.ckm])[0]
         self._bx2 = 2.0 * self.therm_func_b(
             [self._mass / M_SUN_CGS, self._vejecta / self.ckm])[0]
@@ -65,7 +79,10 @@ class RProcess(Engine):
         ]
 
         self._lscale = self._mass * 4.0e18 * 0.36
-        luminosities = [
+        
+	# This is 2017LRR....20....3M: Eqn 22, where the Barnes functions are
+	# implemented as e_th (thermalization efficency)
+	luminosities = [
             self._lscale * (0.5 - IPI * np.arctan(
                 (t * DAY_CGS - 1.3) / 0.11)) ** 1.3 *
             (np.exp(-self._a * t) + np.log1p(
