fd5baa9f18f18ce84987a2e461d85aae762d22e9
Kamile Lukosiute <klukosiu@wellesley.edu>
2018-06-19 17:28:55 -0500

    added comments to diffusion

--- mosfit/modules/transforms/diffusion.py
+++ mosfit/modules/transforms/diffusion.py
@@ -18,30 +18,42 @@ class Diffusion(Transform):
     TRAP_CONST = 3.0 * M_SUN_CGS / (FOUR_PI * KM_CGS ** 2)
 
     _REFERENCES = [
-        {'bibcode': '1982ApJ...253..785A'}
+        {'bibcode': '1982ApJ...253..785A'},
+	{'bibcode': '2017ApJ...850...55N'}
     ]
 
     def process(self, **kwargs):
         """Process module."""
+        # Inputs
         Transform.process(self, **kwargs)
         self._kappa = kwargs[self.key('kappa')]
         self._kappa_gamma = kwargs[self.key('kappagamma')]
         self._m_ejecta = kwargs[self.key('mejecta')]
         self._v_ejecta = kwargs[self.key('vejecta')]
 
+	# Chracteristic time  (Eqn 19 in Arnett 1982, Eqn 6 in Nicholl et al)
+	# Photon diffusion time, depends on characteristics of matter
         self._tau_diff = np.sqrt(self.DIFF_CONST * self._kappa *
                                  self._m_ejecta / self._v_ejecta) / DAY_CGS
+
+	# "Leakage parameter, A" (Eqn 7 Nicholl et al.) 
+	# The leakage term (1 - e(-At^2)) controls the fraction of the magnetar
+	# input that that is thermalized in the ejecta
         self._trap_coeff = (
             self.TRAP_CONST * self._kappa_gamma * self._m_ejecta /
             (self._v_ejecta ** 2)) / DAY_CGS ** 2
         td2, A = self._tau_diff ** 2, self._trap_coeff  # noqa: F841
 
+
+	# Initialize new luminosities array, which will hopefully return Eqn. 5
+	# in Nicholl et al.
         new_lums = np.zeros_like(self._times_to_process)
         if len(self._dense_times_since_exp) < 2:
             return {self.dense_key('luminosities'): new_lums}
         min_te = min(self._dense_times_since_exp)
         tb = max(0.0, min_te)
-        linterp = interp1d(
+        
+	linterp = interp1d(
             self._dense_times_since_exp, self._dense_luminosities, copy=False,
             assume_sorted=True)
 
@@ -68,7 +80,13 @@ class Diffusion(Transform):
         int_args[np.isnan(int_args)] = 0.0
 
         uniq_lums = np.trapz(int_args, int_times)
-        uniq_lums *= -2.0 * np.expm1(-A / int_te2s) / td2
+        
+	# Okay so it calculates the luminosity as prescribed in Eq 5 of Nicholl
+	# et al for some subset of the luminosities? (Unclear, needs analysis.)
+	
+	# np.expm1 is exp(x) -1 (the eqn is 1-exp(x) but the - is thrown into
+	# the -2.
+	uniq_lums *= -2.0 * np.expm1(-A / int_te2s) / td2
 
         new_lums = uniq_lums[np.searchsorted(uniq_times,
                                              self._times_to_process)]
