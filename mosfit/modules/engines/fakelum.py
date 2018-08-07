7134224c1713847a9e2f8b96c15f55069f804d23
klukosiute <klukosiu@wellesley.edu>
2018-08-07 16:04:37 -0400

    DEV: kasen_sed

---
+++ mosfit/modules/engines/fakelum.py
@@ -0,0 +1,25 @@
+"""Definitions for the `FakeLum` class."""
+
+# Important: Only define one ``Module`` class per file.
+
+
+class FakeLum(Engine):
+    """
+    Honestly I'm out of ideas on how to make Kasen SEDs work without
+    luminosities so I'm just going to make some fake ones and see if
+    that helps :(
+    """
+
+    def __init__(self, **kwargs):
+        """Initialize module."""
+        super(Engine, self).__init__(**kwargs)
+    self._wants_dense = True # This function wants a dense times array
+    
+
+    def process(self, **kwargs):
+
+        self._times = kwargs[self.key('dense_times')]
+        luminosities = [ 0.0 for t in self._times]
+        
+
+        return {self.dense_key('luminosities'): luminosities}
